348761c1e8ec8552553d400f26d3dbe970e0ed7e
Christophe <da.costa.christophe.mail@gmail.com>
2026-01-10 15:50:23 +0100

    chore(release): Update version to 1.2.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -72,8 +72,8 @@ android {
         applicationId = "com.cdcoding.securewallet"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2.0"
     }
     packaging {
         resources {
